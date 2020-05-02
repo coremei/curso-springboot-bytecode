564049f63e15a4ac04f3a79579f8de539743477d
Coremei Home <coremei.desarrollador@gmail.com>
2020-05-02 18:19:33 -0500

    Creación inicial desde eclipse

---
+++ curso-springboot-bytecode/src/main/java/com/coremei/sboot/CursoSpringbootBytecodeApplication.java
@@ -0,0 +1,13 @@
+package com.coremei.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringbootBytecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringbootBytecodeApplication.class, args);
+	}
+
+}
